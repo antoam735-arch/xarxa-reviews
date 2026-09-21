7cc3f165350952566e4fd6b12896fc434390b944
Antonio Miguel Martínez Soriano <anto.am735@gmail.com>
2026-09-21 12:56:22 +0200

    chore: bump Xarxa Reviews cache to 2.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'xarxa-reviews-2.2';
+const CACHE = 'xarxa-reviews-2.3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
