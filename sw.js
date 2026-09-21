38eca8754d4bac84d38270af623f1e54415a1d96
Antonio Miguel Martínez Soriano <anto.am735@gmail.com>
2026-09-21 12:43:21 +0200

    fix: invalidate stale Xarxa Reviews cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'xarxa-reviews-2.1';
+const CACHE = 'xarxa-reviews-2.2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
